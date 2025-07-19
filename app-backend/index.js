c7f14ab210785ae4798291e26036950bdf4beb70
rhythmos <ryancmoses@outlook.com>
2025-07-18 21:02:03 -0500

    categorizing data files

--- app-backend/index.js
+++ app-backend/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.json("Hello from the backend of the Time Tracking Dashboard App");
+})
+
 app.get('/api', (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
