af394f9f820419de3f547c5f7671a8a6a60f66c2
rhythmos <ryancmoses@outlook.com>
2025-07-19 20:31:07 -0500

    TTDA-27 Finished endpoints v1

--- app-backend/index.js
+++ app-backend/index.js
@@ -107,16 +107,6 @@ app.get(
   }
 );
 
-app.put(
-  "/api/tasks/:id",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send({
-      message: "Update a task",
-    });
-  }
-);
-
 app.post(
   "/api/tasks",
   passport.authenticate("jwt", { session: false }),
@@ -129,7 +119,7 @@ app.post(
         [req.user.id, title, category]
       );
 
-      const { taskId } = result.rows[0];
+      const { id: taskId } = result.rows[0];
 
       await db.query(
         "INSERT INTO task_entries (task_id, time_spent_minutes) VALUES ($1, $2)",
@@ -223,13 +213,111 @@ app.post("/api/auth/login", async (req, res, next) => {
   })(req, res, next);
 });
 
+app.put(
+  "/api/tasks/:taskId/entries/:entryId",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { taskId, entryId } = req.params;
+    const { category, title, duration } = req.body;
+
+    try {
+      const result = await db.query(
+        `
+        SELECT tasks.id AS task_id,
+          tasks.title AS title,
+          tasks.category AS category,
+          task_entries.id AS entry_id,
+          task_entries.time_spent_minutes AS duration,
+          task_entries.created_at 
+        FROM
+          tasks
+        LEFT JOIN task_entries ON tasks.id = task_entries.task_id
+        WHERE tasks.user_id = $1 AND tasks.id = $2 AND task_entries.id = $3
+        ORDER BY tasks.id, task_entries.created_at DESC
+        `,
+        [req.user.id, taskId, entryId]
+      );
+
+      if (result.rowCount !== 1) {
+        throw new Error("Expected exactly one matching task-entry pair.");
+      }
+
+      if (result.rows.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Task not found or not authorized." });
+      }
+
+      const task = result.rows[0];
+
+      const newCategory = category || task.category;
+      const newTitle = title || task.title;
+      const newDuration = duration || task.duration;
+
+      const shouldUpdateTask =
+        (newCategory !== undefined && newCategory !== task.category) ||
+        (title !== undefined && title !== task.title);
+
+      const shouldUpdateEntry =
+        task.entry_id &&
+        newDuration !== undefined &&
+        newDuration !== null &&
+        newDuration !== 0 &&
+        newDuration !== task.duration;
+
+      await db.query("BEGIN");
+
+      if (shouldUpdateTask) {
+        await db.query(
+          `UPDATE tasks 
+                      SET title = $1, category = $2
+                      WHERE id = $3 AND (title IS DISTINCT FROM $1 OR
+                      category IS DISTINCT FROM $2)`,
+          [newTitle, newCategory, taskId]
+        );
+      }
+
+      if (shouldUpdateEntry) {
+        await db.query(
+          `UPDATE task_entries
+                      SET time_spent_minutes = $1
+                      WHERE id = $2 AND (time_spent_minutes IS DISTINCT FROM $1)`,
+          [newDuration, task.entry_id]
+        );
+      }
+
+      await db.query("COMMIT");
+      res.status(200).send({ message: "Task Updated Successfully!" });
+    } catch (err) {
+      console.error(err);
+      await db.query("ROLLBACK");
+      res.status(500).send({ message: "Internal Server Error" });
+    }
+  }
+);
+
 app.delete(
   "/api/tasks/:id",
   passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send({
-      message: "Deleting a task",
-    });
+  async (req, res) => {
+    try {
+      const result = await db.query(
+        "DELETE FROM tasks WHERE id = $1 RETURNING *",
+        [req.params.id]
+      );
+
+      if (result.rows === 0) {
+        return res.status(404).send({ message: "Task Not Found" });
+      }
+
+      res.status(200).send({
+        status: "200 OK",
+        message: "Task Deleted!",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
