c49a020d055089f74718eaff06c36bd25133e7e5
rhythmos <ryancmoses@outlook.com>
2025-07-23 17:50:35 -0500

    fix: awiat async function

--- app-backend/index.js
+++ app-backend/index.js
@@ -1,10 +1,11 @@
 import initApp from "./app.js";
+import logger from "./config/logger.js";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-  const app = initApp();
+  const app = await initApp();
 
   app.listen(PORT, () => {
     logger.info(`Backend running on http://localhost:${PORT}`);
