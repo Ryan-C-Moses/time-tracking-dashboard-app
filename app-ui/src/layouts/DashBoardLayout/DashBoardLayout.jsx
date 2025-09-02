95553c2f747329853be73107860ecfa342e52633
rhythmos <ryancmoses@outlook.com>
2025-09-02 19:42:50 -0400

    added padding

--- app-ui/src/layouts/DashBoardLayout/DashBoardLayout.jsx
+++ app-ui/src/layouts/DashBoardLayout/DashBoardLayout.jsx
@@ -1,5 +1,5 @@
 const DashBoardLayout = () => {
-    return <div className="text-white">Layout for Dashboard Page</div>
+    return <div className="text-white border-2 border-green-300">Layout for Dashboard Page</div>
 }
 
 export default DashBoardLayout;
--- app-ui/src/pages/DashBoard.jsx
+++ app-ui/src/pages/DashBoard.jsx
@@ -1,7 +1,7 @@
 import DashBoardLayout from "../layouts/DashBoardLayout/DashBoardLayout";
 
 const DashBoard = () => {
-    return <div className="border border-red-600 w-full h-screen">
+    return <div className="border border-red-600 w-full h-screen px-[24px] py-[81px]">
         <DashBoardLayout />
     </div>;
 }
